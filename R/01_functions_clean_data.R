07f127d82734417d90a4c0becd8dd6d03fa03950
Cole Brookson <cole.brookson@gmail.com>
2022-12-13 22:56:46 -0500

    data update

---
+++ R/01_functions_clean_data.R
@@ -0,0 +1,11 @@
+##' File Description
+##' AUTHOR: Cole B. Brookson
+##' DATE OF CREATION: 2022-10-14
+#'
+#' This targets file contains all functions to load, and clean data for this 
+#' project
+#'
+#'All functions are documented using the roxygen2 framework and the docstring
+#'library
+#'
+
