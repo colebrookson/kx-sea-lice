ead487cd1ed1e08796a7543c74911cc7467f9854
Cole Brookson <cole.brookson@gmail.com>
2022-12-14 00:47:53 -0500

    function not done

--- R/01_functions_clean_data.R
+++ R/01_functions_clean_data.R
@@ -9,3 +9,32 @@
 #'library
 #'
 
+library(readxl)
+
+
+multiplesheets <- function(fname) {
+  #' Pulls in multiple excel sheets and collapses into a single object
+  #' 
+  #' @description 
+  #' 
+  #' @param n_spp integer. The number of species at hand.
+  #' @param richness_shape character. The shape of the network. Can take on 
+  #' values of "top-heavy", "bottom-heavy" or "uniform"
+  #' @param rates_df data.frame. Dataframe from pre-analysis body sizes
+  #' with the information on rates from each trophic level
+  #'  
+  #' @usage define_species_list(20, "top-heavy")
+  #' @return List of the species for the present network
+  #' 
+  
+  # getting info about all excel sheets
+  sheets <- readxl::excel_sheets(fname)
+  tibble <- lapply(sheets, function(x) readxl::read_excel(fname, sheet = x))
+  data_frame <- lapply(tibble, as.data.frame)
+  
+  # assigning names to data frames
+  names(data_frame) <- sheets
+  
+  # print data frame
+  print(data_frame)
+}
