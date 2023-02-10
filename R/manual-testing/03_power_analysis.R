4160895a013a8cec312ba73a3d7416e7ee868d54
Cole Brookson <cole.brookson@gmail.com>
2023-02-10 16:25:16 -0500

    add in the (failed) first parallel version

--- R/manual-testing/03_power_analysis.R
+++ R/manual-testing/03_power_analysis.R
@@ -222,13 +222,15 @@ saveRDS(c_list, here("./outputs/power-analysis/list-of-dfs.rds"))
 
 # now loop through the data and fit the models =================================
 
-
-
+c_list <- readRDS(here("./outputs/power-analysis/list-of-dfs.rds"))
 
 # empty lists for outer objects
 outer_liklihoods_c <- vector(mode = "list", length = length(c_list))
 
 for(i in seq_len(length(c_list))) {
+  
+  # set start time
+  start_time_out <- Sys.time
   
   # empty lists for inner objects 
   inner_liklihood_null <- vector(mode = "list", length = length(c_list[[1]]))
@@ -236,107 +238,115 @@ for(i in seq_len(length(c_list))) {
   inner_lrt <- vector(mode = "list", length = length(c_list))
   
   for(j in seq_len(length(c_list[[i]]))) {
-    df <- c_list[[i]][[j]]
-    
-    null_mod <- lme4::lmer(survival ~ spawners:river + (1|brood_year/area),
-                           data = df)
-    
-    alt_mod <- 
-  }
-}
-
-
-
-for(c in seq(0, 1, 0.01)) {
-  
-  # set up list for storage
-  c_list <- vector(mode = "list", length = 1000)
-  
-  # number of times this happens
-  for(i in 1:1000) {
     
     start_time <- Sys.time()
-    # make dataframe to fill
-    c_level_df <- pink_sr
-    c_level_df$survival <- as.numeric(NA)
     
-    # years we have data for
-    for(curr_year in ##################### year loop ###########################
-        c(min(pink_sr$brood_year):max(pink_sr$brood_year))) {
-      # set the year random effect value
-      year_re <- rnorm(n = 1, mean = 0, sd = sd_year) 
-      
-      for(curr_area in all_areas) { ############### area loop ##################
-        # set the area w/in year random effect value
-        area_re <- rnorm(n = 1, mean = 0, sd = sd_area_year) 
-        
-        # get temporary subset of data 
-        # temp_df <- c_level_df[which(c_level_df$brood_year == 
-        #                               as.character(curr_year) & 
-        #                               c_level_df$area == curr_area), ]
-        
-        # this is looping through what is commented out above as being assigned
-        # to temp_df -- this is just faster
-        for(row in seq_len(
-          nrow(
-            c_level_df[which(c_level_df$brood_year == 
-                             as.character(curr_year) & 
-                             c_level_df$area == curr_area), ])
-            )
-          ) { ####### population loop ###########
-          
-          
-          # calculate survival
-          
-          # temp_df[row, "survival"] <- 
-          
-          # so instead of doing it how it's commented out above, I'm assinging
-          # it straight to the c_level_df, by using the subset as is put above
-          # plus another subset here with "row" the iterator and the "survival"
-          # column, again, it's just faster
-          c_level_df[which(c_level_df$brood_year == as.character(curr_year) & 
-                    c_level_df$area == curr_area), ][[row, "survival"]] <-
-            # r, and b * N(t-2)
-            r - (
-              # set population level density dependence
-              b_i_vals[[paste0("spawners:river", 
-                               # get the population on hand
-                               c_level_df[which(c_level_df$brood_year == 
-                                                  as.character(curr_year) & 
-                                                  c_level_df$area == curr_area), 
-                                          ][[row, "river"]])]]
-              # number of spawners
-              * c_level_df[which(c_level_df$brood_year == 
-                                   as.character(curr_year) & 
-                                   c_level_df$area == curr_area), 
-                           ][[row, "spawners"]]) -
-            # the c term and lice goes here
-            (c * c_level_df[which(c_level_df$brood_year == 
-                                    as.character(curr_year) & 
-                                    c_level_df$area == curr_area), 
-                            ][[row, "lice"]]) +
-            # yearly variation
-            year_re +
-            # area within year variation
-            area_re + 
-            # residual variation 
-            resid_sd
-        }
-        
-        ## the below only needs to be here if we use temp_df which we've shown 
-        ## we don't need to 
-        
-        # # put the survival values in the larger dataframe 
-        # c_level_df[which(c_level_df$brood_year == as.character(curr_year) & 
-        #                    c_level_df$area == curr_area), "survival"] <- 
-        #   # this was just filled in the previous loop
-        #   temp_df[which(temp_df$brood_year == 
-        #                   as.character(curr_year) & 
-        #                   temp_df$area == curr_area), "survival"]
-      }
-    }
+    # extract the dataframe at hand
+    df <- c_list[[i]][[j]]
+    # structure it
+    df$brood_year <- as.factor(df$brood_year)
+    df$area <- as.factor(df$area)
+    df$river <- as.factor(df$river)
     
-    c_list[[i]] <- c_level_df
-    print()
+    # null model 
+    null_mod <- lme4::lmer(survival ~ spawners:river + (1|brood_year/area),
+                           data = df)
+    null_logLik <- stats::logLik(null_mod)
+    
+    # alternative model
+    alt_mod <- lme4::lmer(survival ~ spawners:river + lice +
+                            (1|brood_year/area),
+                          data = df)
+    alt_logLik <- stats::logLik(alt_mod)
+    
+    # do the test
+    teststat <- -2 * (as.numeric(null_logLik) - as.numeric(alt_logLik))
+    df_diff <- attr(alt_logLik, "df") - attr(null_logLik, "df")
+    p_val <- pchisq(teststat, df = df_diff, lower.tail = FALSE)
+    
+    inner_liklihood_null[[j]] <- null_logLik
+    inner_liklihood_alt[[j]] <- alt_logLik
+    inner_lrt[[j]] <- p_val
+    
+    # get a sense for how long
+    out_time <- Sys.time()
+    print((out_time - start_time))
   }
+  
+  outer_liklihoods_c[[i]] <- list(null_like = inner_liklihood_null,
+                                  alt_like = inner_liklihood_alt,
+                                  lrt_pval = inner_lrt)
+  end_time_out <- Sys.time()
+  print(paste0("time of one run = ", (end_time_out - start_time_out)))
 }
+saveRDS(outer_liklihoods_c, here("./outputs/power-analysis/list-of-lrts.rds"))
+
+
+# trying in parallel ===========================================================
+library(parallel)
+library(foreach)
+
+# set up parallel
+cores <- parallel::detectCores() - 1
+cluster <- parallel::makeCluster(cores)
+doParallel::registerDoParallel(cl = cluster)
+
+# check if it's ready
+foreach::getDoParRegistered()
+foreach::getDoParWorkers()
+
+outer_liklihoods_c <- foreach::foreach(
+  i = seq_len(length(c_list)), 
+  .combine = "c") %dopar% {
+  
+  # empty lists for inner objects 
+  inner_liklihood_null <- vector(mode = "list", length = length(c_list[[1]]))
+  inner_liklihood_alt <- vector(mode = "list", length = length(c_list[[1]]))
+  inner_lrt <- vector(mode = "list", length = length(c_list))
+  
+  for(j in seq_len(length(c_list[[i]]))) {
+    
+    start_time <- Sys.time()
+    
+    # extract the dataframe at hand
+    df <- c_list[[i]][[j]]
+    # structure it
+    df$brood_year <- as.factor(df$brood_year)
+    df$area <- as.factor(df$area)
+    df$river <- as.factor(df$river)
+    
+    # null model 
+    null_mod <- lme4::lmer(survival ~ spawners:river + (1|brood_year/area),
+                           data = df)
+    null_logLik <- stats::logLik(null_mod)
+    
+    # alternative model
+    alt_mod <- lme4::lmer(survival ~ spawners:river + lice +
+                            (1|brood_year/area),
+                          data = df)
+    alt_logLik <- stats::logLik(alt_mod)
+    
+    # do the test
+    teststat <- -2 * (as.numeric(null_logLik) - as.numeric(alt_logLik))
+    df_diff <- attr(alt_logLik, "df") - attr(null_logLik, "df")
+    p_val <- pchisq(teststat, df = df_diff, lower.tail = FALSE)
+    
+    inner_liklihood_null[[j]] <- null_logLik
+    inner_liklihood_alt[[j]] <- alt_logLik
+    inner_lrt[[j]] <- p_val
+    
+    # get a sense for how long
+    out_time <- Sys.time()
+    print((out_time - start_time))
+  
+  }
+  return(list(null_like = inner_liklihood_null,
+              alt_like = inner_liklihood_alt,
+              lrt_pval = inner_lrt))
+}
+saveRDS(outer_liklihoods_c, here("./outputs/power-analysis/list-of-lrts.rds"))
+
+
+
+
+parallel::stopCluster(cl = cluster)
