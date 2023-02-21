2fd3031a9459a69d0e818a7b725c364fa9a97097
Cole Brookson <cole.brookson@gmail.com>
2023-02-21 03:06:49 -0500

    ?

--- R/CC/run-power-analysis.sh
+++ R/CC/run-power-analysis.sh
@@ -14,8 +14,4 @@ module load gcc/11.3.0 r/4.2.1
 
 export R_LIBS=/home/brookson/scratch/.local/R/$EBVERSIONR/
 
-<<<<<<< HEAD
-parallel -j $SLURM_CPUS_PER_TASK 'Rscript ~/scratch/kx-sea-lice/R/CC/00_power_analysis.R {}' ::: {1..2}
-=======
 parallel -j $SLURM_CPU_PER_TASK 'Rscript ~/scratch/kx-sea-lice/R/CC/00_power_analysis.R' 
->>>>>>> 61203d42b3e1a34b7474bb814495d3deeeacb2d0
