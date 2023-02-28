3da913d3cceb942e66c92fba3849847b5db41590
Cole Brookson <cole.brookson@gmail.com>
2023-02-28 13:49:53 -0500

    get rid of new line problem

--- R/CC/make-glost-files.sh
+++ R/CC/make-glost-files.sh
@@ -11,22 +11,15 @@ do
     fi
     for ((j=$i; j<=$var; j++))
     do
-        envname='${nargument}'
-        echo "nargument=${j} && Rscript /home/brookson/scratch/kx-sea-lice/R/CC/00_power_analysis.R ${envname}" >> ~/Github/kx-sea-lice/R/CC/power-analysis-tasks-${i}-${var}.txt 
-    done
-
-done
-for i in {97..993..32}
-do
-    if [ "${i}" -lt "993" ] 
-    then
-        var=$(($i + 31))
-    fi
-    if [ "${i}" -eq "993" ]
-    then 
-        var=$(($i + 7))
-    fi
-    do
-        sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' ~/Github/kx-sea-lice/R/CC/power-analysis-tasks-${i}-${var}.txt
+        if [ "${j}" -lt "$var" ] # this is because the last line can't be empty for the GLOST to work properly apparently
+        then
+            envname='${nargument}'
+            echo "nargument=${j} && Rscript /home/brookson/scratch/kx-sea-lice/R/CC/00_power_analysis.R ${envname}" >> ~/Github/kx-sea-lice/R/CC/power-analysis-tasks-${i}-${var}.txt
+        fi
+        if [ "${j}" -eq "$var" ]
+        then
+            envname='${nargument}'
+            echo -n "nargument=${j} && Rscript /home/brookson/scratch/kx-sea-lice/R/CC/00_power_analysis.R ${envname}" >> ~/Github/kx-sea-lice/R/CC/power-analysis-tasks-${i}-${var}.txt 
+        fi
     done
 done
