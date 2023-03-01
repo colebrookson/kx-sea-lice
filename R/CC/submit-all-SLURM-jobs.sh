dbce9c8930c8a14158e9df6d8ebd5f53a13d9a2e
Cole Brookson <cole.brookson@gmail.com>
2023-03-01 00:40:58 -0500

    problem in submit all jobs fixed

--- R/CC/submit-all-SLURM-jobs.sh
+++ R/CC/submit-all-SLURM-jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for i in {65..961..32}
+for i in {63..995..31}
 do 
-    j=$((${i} + 31))
+    j=$((${i} + 30))
     sbatch /home/brookson/scratch/kx-sea-lice/R/CC/run-power-analysis-${i}-${j}.sh
 done
