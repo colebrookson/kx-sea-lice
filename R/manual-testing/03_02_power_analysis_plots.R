aca2cf209ba29782fc24621687b7c27e597921de
Cole Brookson <cole.brookson@gmail.com>
2023-03-06 17:41:46 -0500

    basic plot

--- R/manual-testing/03_02_power_analysis_plots.R
+++ R/manual-testing/03_02_power_analysis_plots.R
@@ -14,3 +14,31 @@ library(here)
 library(magrittr)
 library(dplyr)
 library(ggplot2)
+library(ggthemes)
+
+all_power_sims <- readr::read_csv(here::here("./outputs/power-analysis/all-power-analysis-runs.csv"))
+
+all_power_sims <- all_power_sims %>% 
+  # add in a one/zero for if the p value is < 0.05
+  dplyr::rowwise() %>% 
+  dplyr::mutate(
+    better = ifelse(p <= 0.05, 1, 0)
+  ) %>% 
+  dplyr::group_by(c) %>% 
+  dplyr::summarize(
+    n = n(), 
+    count = sum(better)
+  ) %>% 
+  dplyr::mutate(
+    prop = count / n
+  )
+
+ggplot(data = all_power_sims) + 
+  geom_point(aes(x = c, y = prop)) + 
+  labs(x = "C", y = "Power") + 
+  ggthemes::theme_base() + 
+  geom_vline(aes(xintercept = 0.19), linetype = "dashed", colour = "red") + 
+  geom_hline(aes(
+    yintercept = all_power_sims[which(all_power_sims$c == 0.19), "prop"][[1]]
+    ), linetype = "dashed", colour = "red")
+
