ae19c601c86d44badbe4313f03456ce7439320db
colebrookson <cole.brookson@gmail.com>
2023-04-22 20:45:50 -0400

    trying to make the opposite polygon

--- R/manual-testing/02_01_buffer_mapping.R
+++ R/manual-testing/02_01_buffer_mapping.R
@@ -8,6 +8,15 @@ library(raster)
 library(here)
 
 geo_data <- readRDS(here("./data/geo-spatial/gadm36_CAN_1_sp.rds"))
+geo_data_sf <- st_as_sf(geo_data)
+
+geo_data_sf_bc <- geo_data_sf[which(geo_data_sf$NAME_1 == "British Columbia"),]
+non_land <- sf::st_difference(geo_data_sf_bc)
+
+ggplot() + 
+  geom_sf(data = non_land, color = 'blue', fill = NA) 
+
+
 
 province = "British Columbia"
 canada_prov = geo_data[geo_data$NAME_1 %in% province] # subset to just BC
@@ -110,3 +119,66 @@ sf_airports_polygons <- st_polygonize(sf_airports)
 
 # data from stamen
 library(ggmap)
+
+
+# Stack overflow example =======================================================
+library(tidyverse)
+library(sf)
+library(sfnetworks)
+library(nngeo)
+
+# set seed to make the script reproducible,
+#  since there is random sampling used
+set.seed(813)
+
+# Getting your data:
+x <- dget("https://raw.githubusercontent.com/BajczA475/random-data/main/Moose.lake")
+# Subset to get just one lake
+moose_lake <- x[5,]
+boat_ramp <- dget("https://raw.githubusercontent.com/BajczA475/random-data/main/Moose.access")
+sample_locations <- dget("https://raw.githubusercontent.com/BajczA475/random-data/main/Moose.ssw")
+sample_bbox <- dget("https://raw.githubusercontent.com/BajczA475/random-data/main/Moose.box")
+
+# sample the bounding box with regular square points, then connect each point to the closest 9 points
+#  8 should've worked, but left some diagonals out.
+sq_grid_sample <- st_sample(st_as_sfc(st_bbox(moose_lake)), 
+                            size = 10000, type = 'regular') %>% st_as_sf() %>%
+  st_connect(.,.,k = 9)
+
+# remove connections that are not within the lake polygon
+sq_grid_cropped <- sq_grid_sample[st_within(sq_grid_sample, moose_lake, sparse = F)]
+
+# make an sfnetwork of the cropped grid
+# make an sfnetwork of the cropped grid
+lake_network <- sq_grid_cropped %>% as_sfnetwork()
+
+# find the (approximate) distance from boat ramp to point 170 (far north)
+pt170 <- st_network_paths(lake_network, 
+                          from = boat_ramp,
+                          to = sample_locations[170,]) %>%
+  pull(edge_paths) %>%
+  unlist()
+
+lake_network %>% 
+  activate(edges) %>%
+  slice(pt170) %>%
+  st_as_sf() %>%
+  st_combine() %>%
+  st_length()
+
+
+
+
+ggplot() + 
+  geom_sf(data = sq_grid_sample, alpha = .05) +
+  geom_sf(data = sq_grid_cropped, color = 'dodgerblue') + 
+  geom_sf(data = moose_lake, color = 'blue', fill = NA) +
+  geom_sf(data = boat_ramp, color = 'springgreen', size = 4) + 
+  geom_sf(data = sample_locations[170,], color = 'deeppink1', size = 4) +
+  geom_sf(data = lake_network %>% 
+            activate(edges) %>%
+            slice(pt170) %>%
+            st_as_sf(),
+          color = 'turquoise',
+          size = 2) +
+  theme_void()
