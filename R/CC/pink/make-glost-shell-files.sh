53b16952a45b6b850b2b0629cb697558a1f5c321
Cole Brookson <cole.brookson@gmail.com>
2023-04-24 22:31:35 -0400

    update shell scripts

--- R/CC/pink/make-glost-shell-files.sh
+++ R/CC/pink/make-glost-shell-files.sh
@@ -27,5 +27,5 @@ module load StdEnv/2020 gcc/9.3.0 openmpi/4.0.3 glost/0.3.1 r/4.2.1
 
 export R_LIBS=/home/brookson/scratch/.local/R/$var/
 
-srun glost_launch /home/brookson/scratch/kx-sea-lice/R/CC/power-analysis-tasks-${i}-${j}.txt" >> ~/Github/kx-sea-lice/R/CC/run-power-analysis-${i}-${j}.sh 
+srun glost_launch /home/brookson/scratch/kx-sea-lice/R/CC/pink/power-analysis-tasks-${i}-${j}.txt" >> ~/Github/kx-sea-lice/R/CC/pink/run-power-analysis-${i}-${j}.sh 
 done 
