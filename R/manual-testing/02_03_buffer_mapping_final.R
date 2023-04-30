eb86cee5243f243a6c71a3fbe0e6882619247084
Cole Brookson <cole.brookson@gmail.com>
2023-04-29 20:05:24 -0400

    should be ready to do all the shortest paths

--- R/manual-testing/02_03_buffer_mapping_final.R
+++ R/manual-testing/02_03_buffer_mapping_final.R
@@ -74,14 +74,14 @@ non_land_study <- sf::st_crop(non_land, xmin = -128.85,
                               ymax = 53.1)
 
 # cut to just the kid bay farm and area
-kid_bay_area <- sf::st_crop(non_land, xmin = -128.6,
-                            xmax = -128.12, ymin = 52.52, 
-                            ymax = 53.05)
+upper_area <- sf::st_crop(non_land, xmin = -128.6,
+                            xmax = -128.12, ymin = 52.5, 
+                            ymax = 52.9)
 
 # make sure the projection is the same (UTM)
 utm_geo_data <- st_transform(non_land_study, 
                              crs="+proj=utm +zone=9 +datum=NAD83 +unit=m")
-utm_kid_bay <- st_transform(kid_bay_area, 
+utm_upper_area <- st_transform(upper_area, 
                             crs="+proj=utm +zone=9 +datum=NAD83 +unit=m")
 
 # quick sanity check for what we're looking at 
@@ -89,7 +89,7 @@ ggplot() +
   geom_sf(data = utm_geo_data, color = 'black', fill = "grey90") +
   theme_base()
 ggplot() + 
-  geom_sf(data = utm_kid_bay, color = 'black', fill = "grey90") +
+  geom_sf(data = utm_upper_area, color = 'black', fill = "grey90") +
   theme_base()
 
 # now, we can do this one farm region at a time, or all at once, I think it's 
@@ -98,20 +98,23 @@ ggplot() +
 ## make the grid sample on the kid-bay study region ============================
 
 # first make it as small as possible
-kid_grid_sample <- sf::st_sample(sf::st_as_sfc(sf::st_bbox(utm_kid_bay)), 
-                               # the size is really large to make a fine grid
-                               size = 200000, type = 'regular') %>% 
-sf::st_as_sf() %>%
-nngeo::st_connect(.,.,k = 9) 
-saveRDS(kid_grid_sample, here("./outputs/geo-objs/kid-bay-grid-sample.rds"))
+upper_area_grid_sample <- sf::st_sample(
+  sf::st_as_sfc(sf::st_bbox(utm_upper_area)),
+  # the size is really large to make a fine grid
+  size = 50000, type = 'regular') %>% 
+  sf::st_as_sf() %>%
+  nngeo::st_connect(.,.,k = 9) 
+saveRDS(upper_area_grid_sample, 
+        here("./outputs/geo-objs/upper-area-grid-sample.rds"))
 
 # remove connections that are not within the water polygon
 crop_time_start <- Sys.time()
 
-grid_cropped <- kid_grid_sample[sf::st_contains(
-utm_kid_bay, kid_grid_sample, sparse = F)]
+upper_area_grid_cropped <- upper_area_grid_sample[sf::st_contains(
+  utm_upper_area, upper_area_grid_sample, sparse = F)]
 #rm(kid_grid_sample); gc()
-saveRDS(grid_cropped, here("./outputs/geo-objs/grid-sample-cropped.rds"))
+saveRDS(upper_area_grid_cropped, 
+        here("./outputs/geo-objs/upper-area-grid-sample-cropped.rds"))
 
 crop_time <- Sys.time() - crop_time_start
 print(paste0("For the cropping, elapsed time: ", round(crop_time, 2), 
@@ -119,10 +122,12 @@ print(paste0("For the cropping, elapsed time: ", round(crop_time, 2),
 
 # now actually make the network
 net_time_start <- Sys.time()
-network <- as_sfnetwork(grid_cropped, directed = FALSE) %>% 
-activate("edges") %>% 
-mutate(weight = edge_length()) 
-saveRDS(network, here("./outputs/geo-objs/network.rds"))
+
+upper_network <- as_sfnetwork(upper_area_grid_cropped, directed = FALSE) %>% 
+  activate("edges") %>% 
+  mutate(weight = edge_length()) 
+saveRDS(upper_network, here("./outputs/geo-objs/upper-area-network.rds"))
+
 net_time <- Sys.time() - net_time_start
 print(paste0("For the network creation, elapsed time: ", round(net_time, 2), 
            " minutes"))
@@ -132,33 +137,158 @@ print(paste0("For the network creation, elapsed time: ", round(net_time, 2),
 # each of the "from" points, to all of the other points on the entire grid
 # this will take a LONG time
 
-# get the shortest paths 
-paths_start <- Sys.time()
-all_paths <- sfnetworks::st_network_paths(
-  x = network,
-  from = farms_utm, 
+
+# subset into the paths from each of the farms since that's how I'll need to 
+# plot them
+kid_bay <- farms_utm[which(farms_utm$site == "Kid Bay"),]
+loch <- farms_utm[which(farms_utm$site == "Lochalsh"),]
+goat <- farms_utm[which(farms_utm$site == "Goat Cove"),]
+sheep <- farms_utm[which(farms_utm$site == "Sheep Passage"),]
+lime <- farms_utm[which(farms_utm$site == "Lime Point"),]
+
+### kid bay ==================================================================== 
+kid_paths_start <- Sys.time()
+kid_paths <- sfnetworks::st_network_paths(
+  x = upper_network,
+  from = kid_bay, 
   weights = "weight"
 )  
-saveRDS(all_paths, here("./outputs/geo-objs/all-paths.rds"))
-path_time <- Sys.time() - paths_start
-print(paste0("To calculate the paths, elapsed time: ", round(path_time, 2), 
+saveRDS(kid_paths, here("./outputs/geo-objs/kid-bay-paths.rds"))
+kid_paths_time <- Sys.time() - kid_paths_start
+print(paste0("To calculate the kid paths, elapsed time: ", 
+             round(kid_paths_time, 2), 
+             " minutes"))
+### goat cove ==================================================================
+goat_paths_start <- Sys.time()
+goat_paths <- sfnetworks::st_network_paths(
+  x = upper_network,
+  from = goat, 
+  weights = "weight"
+)  
+saveRDS(goat_paths, here("./outputs/geo-objs/kid-bay-paths.rds"))
+goat_paths_time <- Sys.time() - goat_paths_start
+print(paste0("To calculate the goat paths, elapsed time: ", 
+             round(goat_paths_time, 2), 
              " minutes"))
 
+### sheep passage ==============================================================
+sheep_paths_start <- Sys.time()
+sheep_paths <- sfnetworks::st_network_paths(
+  x = upper_network,
+  from = sheep, 
+  weights = "weight"
+)  
+saveRDS(sheep_paths, here("./outputs/geo-objs/kid-bay-paths.rds"))
+sheep_paths_time <- Sys.time() - sheep_paths_start
+print(paste0("To calculate the sheep paths, elapsed time: ", 
+             round(sheep_paths_time, 2), 
+             " minutes"))
+
+### lime point =================================================================
+lime_paths_start <- Sys.time()
+lime_paths <- sfnetworks::st_network_paths(
+  x = upper_network,
+  from = lime, 
+  weights = "weight"
+)  
+saveRDS(lime_paths, here("./outputs/geo-objs/kid-bay-paths.rds"))
+lime_paths_time <- Sys.time() - lime_paths_start
+print(paste0("To calculate the lime paths, elapsed time: ", 
+             round(lime_paths_time, 2), 
+             " minutes"))
+
+
+## pull the nodes and the edges ================================================
+
 # now pull both the nodes and the edges 
-nodes_all <- all_paths %>%
+nodes_all_kid <- kid_paths %>%
   pull(node_paths) 
-edges_all <- all_paths %>%
+edges_all_kid <- kid_paths %>%
+  pull(edge_paths) 
+
+nodes_all_goat <- goat_paths %>%
+  pull(node_paths) 
+edges_all_goat <- goat_paths %>%
+  pull(edge_paths) 
+
+nodes_all_sheep <- sheep_paths %>%
+  pull(node_paths) 
+edges_all_sheep <- sheep_paths %>%
+  pull(edge_paths) 
+
+nodes_all_lime <- lime_paths %>%
+  pull(node_paths) 
+edges_all_lime <- lime_paths %>%
   pull(edge_paths) 
 
 ## filter the edges ============================================================
 
 # here, use our pre-defined function to figure out which of the edges match our 
 # criteria of <30km
-short_start <- Sys.time()
-short_edges <- len_crit(net = network, edges = edges_all)
-saveRDS(short_edges, here("./outputs/geo-objs/short-edges.rds"))
-short_time <- Sys.time() - short_start
-print(paste0("To calculate the paths, elapsed time: ", round(short_time, 2), 
+
+### kid bay ====================================================================
+kid_short_start <- Sys.time()
+cl <- parallel::makeCluster(8)
+
+parallel::clusterEvalQ(cl, {library(dplyr); library(sfnetworks); 
+  library(magrittr); library(sf)})
+parallel::clusterExport(cl, varlist = c("edges_all_kid", "network"))
+
+kid_short_edges <- parSapply(cl, edges_all_kid, slice_fun, net = network)
+parallel::stopCluster(cl)
+
+saveRDS(kid_short_edges, here("./outputs/geo-objs/kid-short-edges.rds"))
+kid_short_time <- Sys.time() - kid_short_start
+print(paste0("To calculate the paths, elapsed time: ", 
+             round(kid_short_time, 2), 
+             " minutes"))
+
+### goat cove ==================================================================
+goat_short_start <- Sys.time()
+cl <- parallel::makeCluster(8)
+
+parallel::clusterEvalQ(cl, {library(dplyr); library(sfnetworks); 
+  library(magrittr); library(sf)})
+parallel::clusterExport(cl, varlist = c("edges_all_goat", "network"))
+
+goat_short_edges <- parSapply(cl, edges_all_goat, slice_fun, net = network)
+parallel::stopCluster(cl)
+saveRDS(goat_short_edges, here("./outputs/geo-objs/goat-short-edges.rds"))
+goat_short_time <- Sys.time() - goat_short_start
+print(paste0("To calculate the paths, elapsed time: ", 
+             round(goat_short_time, 2), 
+             " minutes"))
+
+### sheep passage ==============================================================
+sheep_short_start <- Sys.time()
+cl <- parallel::makeCluster(8)
+
+parallel::clusterEvalQ(cl, {library(dplyr); library(sfnetworks); 
+  library(magrittr); library(sf)})
+parallel::clusterExport(cl, varlist = c("edges_all_sheep", "network"))
+
+sheep_short_edges <- parSapply(cl, edges_all_sheep, slice_fun, net = network)
+parallel::stopCluster(cl)
+saveRDS(sheep_short_edges, here("./outputs/geo-objs/sheep-short-edges.rds"))
+sheep_short_time <- Sys.time() - sheep_short_start
+print(paste0("To calculate the paths, elapsed time: ", 
+             round(sheep_short_time, 2), 
+             " minutes"))
+
+### lime point =================================================================
+lime_short_start <- Sys.time()
+cl <- parallel::makeCluster(8)
+
+parallel::clusterEvalQ(cl, {library(dplyr); library(sfnetworks); 
+  library(magrittr); library(sf)})
+parallel::clusterExport(cl, varlist = c("edges_all_lime", "network"))
+
+lime_short_edges <- parSapply(cl, edges_all_lime, slice_fun, net = network)
+parallel::stopCluster(cl)
+saveRDS(lime_short_edges, here("./outputs/geo-objs/lime-short-edges.rds"))
+lime_short_time <- Sys.time() - lime_short_start
+print(paste0("To calculate the paths, elapsed time: ", 
+             round(lime_short_time, 2), 
              " minutes"))
 
 
