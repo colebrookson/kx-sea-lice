b1e62e493e6bd02087c34073a5ad7470b0fd8ca7
Cole Brookson <cole.brookson@gmail.com>
2023-06-14 12:34:14 -0400

    adding in some code

---
+++ R/manual-testing/04_exposure_categorization.R
@@ -0,0 +1,11 @@
+library(readr)
+library(here)
+library(magrittr)
+library(dplyr)
+library(ggplot2)
+library(lme4)
+library(lubridate)
+library(glmmTMB)
+library(stringr)
+
+exposure_2005
---
+++ R/manual-testing/05_model_fitting_manual.R
@@ -0,0 +1,13 @@
+library(readr)
+library(here)
+library(magrittr)
+library(dplyr)
+library(ggplot2)
+library(lme4)
+library(lubridate)
+library(glmmTMB)
+library(stringr)
+
+farm_lice <- read_csv(here("./data/farm-lice/clean/clean-farm-lice-df.csv"))
+pink_sr_df <- read_csv(here("./data/spawner-recruit/clean/pink-sr-data-clean.csv"))
+wild_lice <- read_csv(here("./data/wild-lice/clean/clean-wild-lice-df.csv"))
