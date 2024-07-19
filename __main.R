77d399a774e7fba6501f3877ba9658219e20ff8f
colebrookson <cole.brookson@gmail.com>
2024-07-19 15:25:00 -0400

    regressions

--- __main.R
+++ __main.R
@@ -12,9 +12,11 @@
 
 #' There are a handful of major data components over a few different sources
 #' that are cleaned here:
-#'      1. data on sea lice on wild fish
-#'      2. data on sea lice on farmed fish
+#'      1. data on sea lice on wild fish -- `wild_lice`
+#'      2. data on sea lice on farmed fish -- `farm_lice`
 #'      3. stock-recruit data for wild chum and pink salmon
-#'      4. geospatial data on where the various fish stocks are
+#'             `all_pinks_rivers`
+#'             `all_chums_rivers`
+#'      4. geospatial data on stock locations -- `sr_pop_sites`
 
 source(here::here("./src/R/00_data_cleaning.R"))
---
+++ src/R/01_lice_regressions.R
@@ -0,0 +1,14 @@
+##' File Description
+##' AUTHOR: Cole B. Brookson
+##' DATE OF CREATION: 2024-08-19
+#'
+#' Code to clean all the data using pre-written functions
+#'
+#' All functions are documented using the roxygen2 framework and the docstring
+#' library
+#'
+
+library(magrittr)
+library(ggplot2)
+source(here::here("./src/R/functions/00_functions_global.R"))
+source(here::here("./src/R/functions/02_functions_lice_v_lice_regression.R"))
